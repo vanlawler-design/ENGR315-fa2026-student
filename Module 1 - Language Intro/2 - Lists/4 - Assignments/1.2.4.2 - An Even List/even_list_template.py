306658df103388e2614c03ad0dfe67cf2c4c25b8
vanlawler <vanlawler@gmail.com>
2026-09-09 16:29:49 -0400

    1.2.4.1-2

--- Module 1 - Language Intro/2 - Lists/4 - Assignments/1.2.4.2 - An Even List/even_list_template.py
+++ Module 1 - Language Intro/2 - Lists/4 - Assignments/1.2.4.2 - An Even List/even_list_template.py
@@ -22,7 +22,7 @@ YOUR CODE BEGINS BELOW HERE. FILL IN THE MISSING OPERATIONS / CODE
 
 
 # this is the final result. Modify this line, and the empty lines above, to solve the assignment
-middle_average = None
+middle_average = (even_list[len(even_list) // 2] + even_list[(len(even_list) // 2) - 1]) / 2
 
 # the average of middle elements is
 print("The average is: ", middle_average)
