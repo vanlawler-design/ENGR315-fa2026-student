1184b6f3f8bf113c2dae5618410baec2545e6385
vanlawler-design <vanlawler@gmail.com>
2026-09-18 14:45:37 -0400

    1.3.4assignments

--- Module 1 - Language Intro/3 - Conditionals/4 - Assignments/1.3.4.1 - Deviation Lists/deviation_lists_template.py
+++ Module 1 - Language Intro/3 - Conditionals/4 - Assignments/1.3.4.1 - Deviation Lists/deviation_lists_template.py
@@ -23,6 +23,8 @@ random_list_B = random.sample(range(100), random_length)
 # use the std() method from numpy to determine which list has the largest standard deviation
 
 ### YOUR CODE HERE
+std_A = np.std(random_list_A)
+std_B = np.std(random_list_B)
 
 # set this variable equal to the list with the largest standard deviation
 # do not modify this variable's name, you can/should adjust the contents ;)
@@ -30,3 +32,8 @@ random_list_B = random.sample(range(100), random_length)
 longest_list_is = None
 
 ### YOUR CODE HERE
+
+if std_A > std_B:
+    longest_list_is = random_list_A
+else:
+    longest_list_is = random_list_B
--- Module 1 - Language Intro/3 - Conditionals/4 - Assignments/1.3.4.2 - Odds and Evens Count/odd_evens_count_template.py
+++ Module 1 - Language Intro/3 - Conditionals/4 - Assignments/1.3.4.2 - Odds and Evens Count/odd_evens_count_template.py
@@ -29,3 +29,8 @@ num_evens = 0
 num_odds = 0
 
 ### YOUR CODE BEGINS HERE ###
+for num in nums:
+    if num % 2 == 0:
+        num_evens += 1
+    else:
+        num_odds += 1
